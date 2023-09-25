f0e05ada96b534db0c372db3aea8040d42c10f3c
Baptiste Dumas <baptiste.dumas@epfedu.fr>
2023-09-25 11:10:58 +0200

    Création d'un nouveau projet versionné

---
+++ CALCULATOR/src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+/**
+ * Auteur : Baptiste Dumas
+ * Date de création : 25/09/2023
+ */
+import java.util.Scanner;
+
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Please enter the operator:");
+            System.out.println("1) add");
+            System.out.println("2) subtract");
+            System.out.println("3) multiply");
+            System.out.println("4) divide");
+            System.out.println("5) modulo");
+            
+        }
+    }
+}
+
+
