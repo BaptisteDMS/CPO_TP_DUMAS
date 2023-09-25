4fad1e6fbe15ccc74bd588721e314d69181ce588
Baptiste Dumas <baptiste.dumas@epfedu.fr>
2023-09-26 00:06:14 +0200

    Création d'un nouveau projet versionné

--- CALCULATOR/src/main/java/Calculator.java
+++ CALCULATOR/src/main/java/Calculator.java
@@ -4,20 +4,66 @@
  */
 import java.util.Scanner;
 
-
 public class Calculator {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Please enter the operator:");
-            System.out.println("1) add");
-            System.out.println("2) subtract");
-            System.out.println("3) multiply");
-            System.out.println("4) divide");
-            System.out.println("5) modulo");
-            
+        Scanner scanner = new Scanner(System.in);
+
+        // Étape 1: Demandez à l'utilisateur de choisir un opérateur
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) subtract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+
+        int operatorChoice = scanner.nextInt();
+
+        // Étape 7: Vérifiez si l'opérateur est valide (entre 1 et 5 inclus)
+        if (operatorChoice < 1 || operatorChoice > 5) {
+            System.out.println("Invalid operator choice. Please choose a valid operator (1-5).");
+            scanner.close();
+            return;
         }
+
+        // Étape 4: Demandez à l'utilisateur les opérandes
+        int[] operands = new int[2];
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Please enter operand " + (i + 1) + ":");
+            operands[i] = scanner.nextInt();
+        }
+
+        int result = 0;
+
+        // Étape 6: Calculez le résultat en fonction de l'opérateur choisi
+        switch (operatorChoice) {
+            case 1:
+                result = operands[0] + operands[1];
+                break;
+            case 2:
+                result = operands[0] - operands[1];
+                break;
+            case 3:
+                result = operands[0] * operands[1];
+                break;
+            case 4:
+                if (operands[1] != 0) {
+                    result = operands[0] / operands[1];
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                    scanner.close();
+                    return;
+                }
+                break;
+            case 5:
+                result = operands[0] % operands[1];
+                break;
+        }
+
+        // Étape 6 (suite): Affichez le résultat
+        System.out.println("The result is: " + result);
+
+        scanner.close();
     }
 }
-
-
