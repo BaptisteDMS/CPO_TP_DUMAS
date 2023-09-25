c4db9f30e17ad63be573644b1c5f39885a9e2a48
Baptiste Dumas <baptiste.dumas@epfedu.fr>
2023-09-25 11:11:23 +0200

    Création d'un nouveau projet versionné

---
+++ EXO2/src/exo2/EXO2.java
@@ -0,0 +1,34 @@
+/*/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+*/
+package exo2;
+
+/**
+ *
+ * @author baptiste
+ */
+
+//<editor-fold defaultstate="collapsed" desc="comment">
+
+public static void main(String[] args) {
+// Déclaration des variables
+int nb; // nombre d'entiers à additionner
+int result; // résultat
+int ind; // indice
+nb = 5;
+result = 0;
+
+// Addition des nb premiers entiers
+ind = 1;
+while (ind <= nb) {
+result = result + ind;
+ind++; // Incrémente la valeur de ind à chaque itération
+}
+
+// Affichage du résultat
+System.out.println();
+System.out.println("La somme des " + nb + " entiers est : " + result);
+}
+*/
+//</editor-fold>
