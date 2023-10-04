4e576ce15313bbadda911be431e95165bcfad1b7
Baptiste Dumas <baptiste.dumas@epfedu.fr>
2023-10-04 14:54:07 +0200

    Création d'un nouveau projet versionné

--- TP1_guessMyNumber_DUMAS/src/TP1_guessMyNumber_DUMAS.java
+++ TP1_guessMyNumber_DUMAS/src/TP1_guessMyNumber_DUMAS.java
@@ -9,29 +9,67 @@ import java.util.Scanner;
 
 public class TP1_guessMyNumber_DUMAS {
     public static void main(String[] args) {
-        //générateur de nombres aléatoires
+        Scanner scanner = new Scanner(System.in);
         Random generateurAleat = new Random();
         
-        // nombre aléatoire entre 0 et 100
+        System.out.println("Choisissez le niveau de difficulte :");
+        System.out.println("1. Facile (nombre d'essais illimit et messages d'aide)");
+        System.out.println("2. Normal (nombre d'essais limite à 10 messages standards)");
+        System.out.println("3. Difficile (nombre d'essais limite a 5, pas de messages )");
+
+        int choixDifficulte = scanner.nextInt();
+        int maxEssais;
+        
+        switch (choixDifficulte) {
+            case 1 -> maxEssais = Integer.MAX_VALUE;
+            case 2 -> maxEssais = 10;
+            case 3 -> maxEssais = 5;
+            default -> {
+                System.out.println("Choix de difficulté invalide. Sélection du mode normal.");
+                maxEssais = 10;
+            }
+        }
+
         int nombreAleatoire = generateurAleat.nextInt(101); // Génère un nombre aléatoire entre 0 et 100
-   
-        try ( 
-                Scanner scanner = new Scanner(System.in)) {
-            // saisi d'un nombre
-            while (true) {
+        int tentatives = 0; // Compteur de tentatives
+
+        try {
+            while (tentatives < maxEssais) {
                 System.out.print("Entrez un nombre entre 0 et 100 : ");
                 int nombreSaisi = scanner.nextInt();
-                
+                tentatives++;
+
                 if (nombreSaisi < nombreAleatoire) {
-                    System.out.println("Trop petit");
+                    System.out.println("Trop petit !");
+                    if (choixDifficulte == 1) {
+                        System.out.println("Vraiment trop petit !");
+                    }
                 } else if (nombreSaisi > nombreAleatoire) {
-                    System.out.println("Trop grand");
+                    System.out.println("Trop grand !");
+                    if (choixDifficulte == 1) {
+                        System.out.println("Vraiment trop grand !");
+                    }
                 } else {
-                    System.out.println("Gagne");
-                    break; // Termine la boucle si gagné
+                    System.out.println("Gagné !");
+                    System.out.println("Nombre de tentatives : " + tentatives);
+                    break;
+                }
+                
+                // Mode cauchemar : 30% de probabilité d'indication incorrecte
+                if (choixDifficulte == 4 && generateurAleat.nextInt(10) < 3) {
+                    if (nombreSaisi < nombreAleatoire) {
+                        System.out.println("Trop grand !");
+                    } else {
+                        System.out.println("Trop petit !");
+                    }
                 }
             }
+            
+            if (tentatives >= maxEssais) {
+                System.out.println("Nombre d'essais epuises. Le nombre recherché etait : " + nombreAleatoire);
+            }
+        } finally {
+            scanner.close();
         }
     }
 }
-    
