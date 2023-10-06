984c03413d98e3343622a6a123ea99f18ece32bd
Baptiste Dumas <baptiste.dumas@epfedu.fr>
2023-10-06 17:46:02 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_DUMAS/src/bieres_dumas/BouteilleBiere.java
+++ TP2_Bieres_DUMAS/src/bieres_dumas/BouteilleBiere.java
@@ -6,28 +6,44 @@ package bieres_dumas;
  */
 public class BouteilleBiere {
     private final String nom;
-    private final double degreAlcool;
+    private final float degreAlcool;
     private final String brasserie;
     private boolean ouverte;
-
-    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+    
+    public BouteilleBiere(String unNom, float unDegre, String uneBrasserie) {
         nom = unNom;
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
-        ouverte = false;
+        ouverte = false; // Par défaut, la bouteille est fermée à la création
     }
 
+    // Méthode pour lire l'étiquette de la bouteille
     public void lireEtiquette() {
         System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degrés)\nBrasserie : " + brasserie);
     }
 
-    public boolean Decapsuler() {
+    // Méthode pour décapsuler la bouteille
+    public boolean decapsuler() {
         if (!ouverte) {
             ouverte = true;
-            return true;
+            return true; // La bouteille a été ouverte avec succès
         } else {
             System.out.println("Erreur : bière déjà ouverte");
-            return false;
+            return false; // La bouteille était déjà ouverte
         }
     }
+
+    // Méthode pour afficher l'état de la bouteille
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+        if (ouverte) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
+    }
+
 }
--- TP2_Bieres_DUMAS/src/tp2_bieres_dumas/TP2_Bieres_DUMAS.java
+++ TP2_Bieres_DUMAS/src/tp2_bieres_dumas/TP2_Bieres_DUMAS.java
@@ -8,21 +8,29 @@ import bieres_dumas.BouteilleBiere;
  */
 public class TP2_Bieres_DUMAS {
 
-    public static void main(String[] args) {
+ public static void main(String[] args) {
+        // Création d'une première bouteille de bière
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0f, "Dubuisson");
 
- 
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
-        BouteilleBiere autreBiere = new BouteilleBiere("Leffe",  6.6, "Abbaye de Leffe");
-        BouteilleBiere troisiemeBiere = new BouteilleBiere("Bière", 5.0, "Brasserie3");
-
+        // Affichage de l'étiquette
         uneBiere.lireEtiquette();
-        autreBiere.lireEtiquette();
-        troisiemeBiere.lireEtiquette();
 
-        uneBiere.Decapsuler();
+        // Décapsuler la première bouteille
+        uneBiere.decapsuler();
 
+        // Affichage de l'état de la première bouteille
         System.out.println(uneBiere);
+
+        // Création d'une seconde bouteille de bière
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6f, "Abbaye de Leffe");
+
+        // Affichage de l'étiquette
+        autreBiere.lireEtiquette();
+
+        // Décapsuler la seconde bouteille
+        autreBiere.decapsuler();
+
+        // Affichage de l'état de la seconde bouteille
         System.out.println(autreBiere);
-        System.out.println(troisiemeBiere);
     }
 }
